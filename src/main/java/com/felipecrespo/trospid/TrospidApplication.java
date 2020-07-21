83aec089091165214a898b1e1b30cef9e218f711
Felipe Crespo Gambade <felipecrespo@pm.me>
2020-07-21 11:45:50 +0200

    Reinit trospid

---
+++ src/main/java/com/felipecrespo/trospid/TrospidApplication.java
@@ -0,0 +1,13 @@
+package com.felipecrespo.trospid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrospidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrospidApplication.class, args);
+	}
+
+}
