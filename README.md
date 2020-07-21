50836615da54db23d0c84128974c2d48640af867
Felipe Crespo Gambade <felipecrespo@pm.me>
2020-07-21 11:23:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# trospid
+ Trospid Initiative
---
+++ src/main/java/com/felipecrespo/TrospidApplication.java
@@ -0,0 +1,13 @@
+package com.felipecrespo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrospidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrospidApplication.class, args);
+	}
+
+}
